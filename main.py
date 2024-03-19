1291502e6c65d84c4244265d1b7f562a30cae4d6
Ed <eduramq@gmail.com>
2024-03-19 16:45:43 -0700

    Added embedding functionality

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
+# The embedding creation algorithm
+embeddings = OpenAIEmbeddings()
 
 
 # Splitting the text in the file into chunks for embeddings
@@ -10,6 +13,7 @@ text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_over
 
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter=text_splitter)
+
 
 for doc in docs:
     print(doc)
